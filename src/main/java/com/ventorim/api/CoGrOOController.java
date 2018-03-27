47bf8e6e571a3b1c73103a78de39edba1407f1bb
Igor Ventorim <igor.ventorim@gmail.com>
2018-03-27 13:13:07 -0300

    update analyze

--- src/main/java/com/ventorim/api/CoGrOOController.java
+++ src/main/java/com/ventorim/api/CoGrOOController.java
@@ -1,13 +1,17 @@
 package com.ventorim.api;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Locale;
-
 import org.cogroo.analyzer.Analyzer;
 import org.cogroo.analyzer.ComponentFactory;
 import org.cogroo.checker.CheckDocument;
 import org.cogroo.checker.GrammarChecker;
+import org.cogroo.text.Chunk;
 import org.cogroo.text.Document;
+import org.cogroo.text.Sentence;
+import org.cogroo.text.SyntacticChunk;
+import org.cogroo.text.Token;
 import org.cogroo.text.impl.DocumentImpl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -40,30 +44,108 @@ public class CoGrOOController {
 		    System.out.println("Document processed in " 
 		            + ((System.nanoTime() - start) / 1000000) + "ms");
 		    
-		    System.out.println(document);
-		
-	    return "ok";
+	    return this.print(document); 
 	}
 	
 	@RequestMapping(value="grammarcheck",method=RequestMethod.GET)
-	public void grammarChecker(@RequestParam(value="texto",required=true) String documentText)
+	public String grammarChecker(@RequestParam(value="texto",required=true) String documentText)
 	{
 		try 
 		{	
 			GrammarChecker gc = new GrammarChecker(factory.createPipe());
 			CheckDocument document = new CheckDocument(documentText);
 			gc.analyze(document);
-			
-			System.out.println(document);
-			
+			System.out.println(document.getMistakes());
+			return document.getMistakesAsString();
 		} catch (IllegalArgumentException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return null;
 		}
 		
 	}
 	
 	
+	/** A utility method that prints the analyzed document to the std output. */
+	  private String print(Document document) {
+	    StringBuilder output = new StringBuilder();
+	
+	    // and now we navigate the document to print its data
+	    for (Sentence sentence : document.getSentences()) {
+	
+	      // Print the sentence. You can also get the sentence span annotation.
+	      output.append("Sentence: ").append(sentence.getText()).append("\n<br>\n");
+	
+	      output.append("  Tokens: \n");
+	
+	      // for each token found...
+	      for (Token token : sentence.getTokens()) {
+	        String lexeme = token.getLexeme();
+	        String lemmas = Arrays.toString(token.getLemmas());
+	        String pos = token.getPOSTag();
+	        String feat = token.getFeatures();
+	
+	        output.append(String.format("    %-10s %-12s %-6s %-10s\n", lexeme,
+	            lemmas, pos, feat));
+	      }
+	
+	      // we can also print the chunks, but printing it is not that easy!
+	      output.append("<br>\n Chunks: ");
+	      for (Chunk chunk : sentence.getChunks()) {
+	        output.append("[").append(chunk.getTag()).append(": ");
+	        for (Token innerToken : chunk.getTokens()) {
+	          output.append(innerToken.getLexeme()).append(" ");
+	        }
+	        output.append("] ");
+	      }
+	      output.append("\n<br>\n");
+	
+	      // we can also print the shallow parsing results!
+	      output.append("  Shallow Structure: ");
+	      for (SyntacticChunk structure : sentence.getSyntacticChunks()) {
+	        output.append("[").append(structure.getTag()).append(": ");
+	        for (Token innerToken : structure.getTokens()) {
+	          output.append(innerToken.getLexeme()).append(" ");
+	        }
+	        output.append("] ");
+	      }
+	      output.append("\n<br>\n");
+	    }
+	
+	    System.out.println(output.toString());
+	    return output.toString();
+	    
+	  }
+	
+	
 	
 	
 }
+
+//for (org.cogroo.text.Sentence sentence : document.getSentences()) 
+//{ // lista de sentenças
+//	  System.out.print(sentence.getStart()+":"+sentence.getEnd());
+//	  System.out.println(sentence.getText()); // texto da sentença
+
+//	  // Tokens
+//	  for (Token token : sentence.getTokens()) { // lista de tokens
+//	    System.out.println(token.getStart()+":"+ token.getEnd()); // caracteres onde o token começa e termina
+//	    System.out.println(token.getLexeme()); // o texto do token
+//	    System.out.println(token.getLemmas()); // um array com os possíveis lemas para o par lexeme+postag
+//	    System.out.println(token.getPOSTag()); // classe morfológica de acordo com o contexto
+//	    System.out.println(token.getFeatures()); // gênero, número, tempo etc
+//	  }
+//
+//	  // Chunks
+//	  for (Chunk chunk : sentence.getChunks()) { // lista de chunks
+//	    System.out.println(chunk.getStart()+":"+chunk.getEnd()); // índice do token onde o chunk começa e do token onde ele termina
+//	    System.out.println(chunk.getTag()); // the chunk tag
+//	    System.out.println(chunk.getTokens()); // a list with the covered tokens
+//	  }
+
+	  // Structure
+//	  for (SyntacticChunk structure : sentence.getSyntacticChunks()) { // lista de SyntacticChunks
+//	    System.out.println(structure.getStart()+":"+structure.getEnd()); // índice do token onde o structure começa e do token onde ele termina
+//	    System.out.println(structure.getTag()); // the structure tag
+//	    System.out.println(structure.getTokens()); // a list with the covered tokens
+//	  }
